2551677d91102a65916de14eb8b353d5731fe65b
LiberalMiser <31970701+LiberalMiser@users.noreply.github.com>
2017-11-19 20:26:10 -0400

    Version from the Demo with Nigel

--- app/src/main/java/com/example/myser/dspotalpha/FeedsFragment.java
+++ app/src/main/java/com/example/myser/dspotalpha/FeedsFragment.java
@@ -4,19 +4,17 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FeedsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FeedsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FeedsFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public FeedsFragment() {
         // Required empty public constructor
@@ -24,8 +22,17 @@ public class FeedsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feeds, container, false);
+        View view = inflater.inflate(R.layout.fragment_feeds, container, false);
+        /*mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);*/
+        return view;
     }
 
 }
